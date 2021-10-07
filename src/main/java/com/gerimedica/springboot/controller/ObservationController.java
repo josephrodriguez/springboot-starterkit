1e6b00e46d4b22766abf908152816655d402e714
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-07 07:28:05 -0400

    add GET getObservationByCode endpoint

--- src/main/java/com/gerimedica/springboot/controller/ObservationController.java
+++ src/main/java/com/gerimedica/springboot/controller/ObservationController.java
@@ -3,9 +3,13 @@ package com.gerimedica.springboot.controller;
 import com.gerimedica.springboot.data.entity.Observation;
 import com.gerimedica.springboot.data.repository.ObservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,5 +21,18 @@ public class ObservationController {
     @GetMapping("/observations")
     public List<Observation> getAll() {
         return observationRepository.findAll();
+    }
+
+    @GetMapping("/observations/{code}")
+    public ResponseEntity<Observation> getObservationByCode(@PathVariable(value = "code") String code)
+        throws ResourceNotFoundException {
+
+        Observation obs =
+                observationRepository
+                        .findByCode(code)
+                        .stream()
+                        .findFirst()
+                        .orElseThrow(() -> new ResourceNotFoundException("Observation not found :: " + code));
+        return ResponseEntity.ok().body(obs);
     }
 }
--- src/main/java/com/gerimedica/springboot/data/repository/ObservationRepository.java
+++ src/main/java/com/gerimedica/springboot/data/repository/ObservationRepository.java
@@ -4,6 +4,10 @@ import com.gerimedica.springboot.data.entity.Observation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ObservationRepository extends JpaRepository<Observation, Long> {
+
+    public List<Observation> findByCode(String code);
 }
