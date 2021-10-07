ac3a7aaedcf238fd340d758f633dc891788f83ea
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-07 06:08:28 -0400

    setup initial configuration

---
+++ src/main/java/com/gerimedica/springboot/GerimedicaApplication.java
@@ -0,0 +1,13 @@
+package com.gerimedica.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerimedicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerimedicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerimedica/springboot/GerimedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerimedica.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerimedicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
