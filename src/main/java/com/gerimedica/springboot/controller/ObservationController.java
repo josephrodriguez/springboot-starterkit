59cb84c74a0e1f982fbb1ed4753cdaea91292097
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-07 07:02:36 -0400

    add GET method

--- src/main/java/com/gerimedica/springboot/controller/ObservationController.java
+++ src/main/java/com/gerimedica/springboot/controller/ObservationController.java
@@ -1,7 +1,9 @@
 package com.gerimedica.springboot.controller;
 
+import com.gerimedica.springboot.data.entity.Observation;
 import com.gerimedica.springboot.data.repository.ObservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,9 @@ public class ObservationController {
 
     @Autowired
     private ObservationRepository observationRepository;
+
+    @GetMapping("/observations")
+    public List<Observation> getAll() {
+        return observationRepository.findAll();
+    }
 }
