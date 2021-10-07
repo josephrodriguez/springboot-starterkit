7514040fe6206f390f41350f10439c284c77ff04
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-07 08:12:01 -0400

    add BATCH controller for file upload and download

--- src/main/java/com/gerimedica/springboot/controller/BatchController.java
+++ src/main/java/com/gerimedica/springboot/controller/BatchController.java
@@ -1,7 +1,22 @@
 package com.gerimedica.springboot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("/api")
 public class BatchController {
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadFile(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity downloadFile(HttpServletRequest request){
+        return ResponseEntity.ok().build();
+    }
 }
