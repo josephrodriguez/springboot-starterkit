070211c965c9d3e864a1c22ebca0553033d377f0
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-07 06:46:52 -0400

    create getters and setters

--- src/main/java/com/gerimedica/springboot/data/entity/Observation.java
+++ src/main/java/com/gerimedica/springboot/data/entity/Observation.java
@@ -35,4 +35,76 @@ public class Observation {
 
     @Column(name = "SORTING_PRIORITY")
     private Integer sortingPriority;
+
+    public long getObservationId() {
+        return observationId;
+    }
+
+    public void setObservationId(long observationId) {
+        this.observationId = observationId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getCodeListCode() {
+        return codeListCode;
+    }
+
+    public void setCodeListCode(String codeListCode) {
+        this.codeListCode = codeListCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
+    public void setDisplayValue(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public Integer getSortingPriority() {
+        return sortingPriority;
+    }
+
+    public void setSortingPriority(Integer sortingPriority) {
+        this.sortingPriority = sortingPriority;
+    }
 }
