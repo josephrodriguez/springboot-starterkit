e09e3c7830e61cfb0e4d102c3b95d87eea219597
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2021-10-20 23:38:51 -0400

    set annotations for CsvDocumentDto class

--- src/main/java/com/josephrodriguez/learning/springboot/dto/csv/CsvDocumentDto.java
+++ src/main/java/com/josephrodriguez/learning/springboot/dto/csv/CsvDocumentDto.java
@@ -1,5 +1,6 @@
 package com.josephrodriguez.learning.springboot.dto.csv;
 
+import com.josephrodriguez.learning.springboot.annotation.CsvColumn;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,26 +8,33 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-//Lombok annotations
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class CsvDocumentDto {
 
+    @CsvColumn(column = "code")
     private String code;
 
+    @CsvColumn(column = "codeListCode", sort = 1)
     private String codeListCode;
 
+    @CsvColumn(column = "source", sort = 1)
     private String source;
 
+    @CsvColumn(column = "displayValue", sort = 2)
     private String displayValue;
 
+    @CsvColumn(column = "longDescription", sort = 3)
     private String longDescription;
 
+    @CsvColumn(column = "from_date", sort = 4)
     private Date fromDate;
 
+    @CsvColumn(column = "to_date", sort = 5)
     private Date toDate;
 
+    @CsvColumn(column = "sortPriority", sort = 6)
     private Integer sortingPriority;
 }
