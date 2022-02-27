e8398122b488165a96ec7dfef95fc32046c3c735
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2022-02-27 10:36:08 -0500

    add Filter component to add the X-Server headers

---
+++ src/main/java/com/josephrodriguez/springboot/starterkit/filters/DefaultHeadersFilter.java
@@ -0,0 +1,28 @@
+package com.josephrodriguez.springboot.starterkit.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetAddress;
+
+@Component
+public class DefaultHeadersFilter implements Filter {
+
+    private static final String XServerHeaderIP = "X-Server-IP";
+    private static final String XServerHeaderName = "X-Server-Name";
+
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader(XServerHeaderIP, InetAddress.getLocalHost().getHostAddress());
+        response.addHeader(XServerHeaderName, InetAddress.getLocalHost().getHostName());
+
+        filterChain. doFilter(servletRequest, servletResponse);
+    }
+}
