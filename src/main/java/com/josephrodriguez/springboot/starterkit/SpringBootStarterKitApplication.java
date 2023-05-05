b4392daced49e42c179a2a58929099261de79d2c
Jose Rodriguez <josemrodriguez89@gmail.com>
2023-05-06 00:16:40 +0300

    Fix http response filters

--- src/main/java/com/josephrodriguez/springboot/starterkit/SpringBootStarterKitApplication.java
+++ src/main/java/com/josephrodriguez/springboot/starterkit/SpringBootStarterKitApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-//@Slf4j
-@ServletComponentScan
 @SpringBootApplication
 public class SpringBootStarterKitApplication {
 
